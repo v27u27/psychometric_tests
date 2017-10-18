72dc1d670a135897d3708457d67cb0cd2ba35709
v27u27 <v27u27@gmail.com>
2017-10-18 06:34:59 +0530

    mapemotions ready complete

--- mapemotions_sinatra/app.rb
+++ mapemotions_sinatra/app.rb
@@ -1,18 +1,156 @@
 require 'rubygems'
 require 'sinatra'
 
-new_cg = -1
+col1 = 0
+col2 = 0
+col3 = 0
+col4 = 0
+col5 = 0
+col6 = 0
+
+pes_score = 0
+aes_score = 0
+ces_score = 0
+
+pes_percent = 0.0
+aes_percent = 0.0
+ces_percent = 0.0
+
+flag = 0
 
 get '/' do
+	erb :index
+end
+
+get '/result' do
+
+	if flag == 0
+		return redirect '/'
+	end
+
 	data = Hash.new
-	data[:new_cg] = new_cg.round(2)
-	new_cg = -1	
-	erb :index, locals: data
+
+	data[:pes_score] = pes_score
+	data[:pes_percent] = pes_percent
+
+	data[:aes_score] = aes_score
+	data[:aes_percent] = aes_percent
+
+	data[:ces_score] = ces_score
+	data[:ces_percent] = ces_percent
+
+	col1 = 0
+	col2 = 0
+	col3 = 0
+	col4 = 0
+	col5 = 0
+	col6 = 0
+
+	pes_score = 0
+	aes_score = 0
+	ces_score = 0
+
+	pes_percent = 0.0
+	aes_percent = 0.0
+	ces_percent = 0.0
+
+	flag = 0
+
+	erb :result, locals: data
 end
 
 post '/cal' do
-	cg = params["cgpa"].to_f
-	sg = params["sgpa"].to_f
-	new_cg = ((cg*3.0)+sg)/4.0
-	return redirect '/'
+
+	col1arr = [3, 11, 19, 24, 35, 36]
+	col2arr = [9, 17, 22, 27, 31, 32]
+	col3arr = [1, 4, 14, 16, 21, 28]
+	col4arr = [8, 10, 12, 18, 25, 29]
+	col5arr = [7, 13, 15, 20, 30, 34]
+	col6arr = [2, 5, 6, 23, 26, 33]
+	
+
+	for i in col1arr
+		if params["ques#{i}"] == "b"
+			col1 += 1
+		elsif params["ques#{i}"] == "c"
+			col1 += 2
+		elsif params["ques#{i}"] == "d"
+			col1 += 3
+		elsif params["ques#{i}"] == "e"
+			col1 += 4
+		end
+	end
+
+	for i in col2arr
+		if params["ques#{i}"] == "b"
+			col2 += 1
+		elsif params["ques#{i}"] == "c"
+			col2 += 2
+		elsif params["ques#{i}"] == "d"
+			col2 += 3
+		elsif params["ques#{i}"] == "e"
+			col2 += 4
+		end
+	end
+
+	for i in col3arr
+		if params["ques#{i}"] == "b"
+			col3 += 1
+		elsif params["ques#{i}"] == "c"
+			col3 += 2
+		elsif params["ques#{i}"] == "d"
+			col3 += 3
+		elsif params["ques#{i}"] == "e"
+			col3 += 4
+		end
+	end
+
+	for i in col4arr
+		if params["ques#{i}"] == "b"
+			col4 += 1
+		elsif params["ques#{i}"] == "c"
+			col4 += 2
+		elsif params["ques#{i}"] == "d"
+			col4 += 3
+		elsif params["ques#{i}"] == "e"
+			col4 += 4
+		end
+	end
+
+	for i in col5arr
+		if params["ques#{i}"] == "b"
+			col5 += 1
+		elsif params["ques#{i}"] == "c"
+			col5 += 2
+		elsif params["ques#{i}"] == "d"
+			col5 += 3
+		elsif params["ques#{i}"] == "e"
+			col5 += 4
+		end
+	end
+
+	for i in col6arr
+		if params["ques#{i}"] == "b"
+			col6 += 1
+		elsif params["ques#{i}"] == "c"
+			col6 += 2
+		elsif params["ques#{i}"] == "d"
+			col6 += 3
+		elsif params["ques#{i}"] == "e"
+			col6 += 4
+		end
+	end
+
+	pes_score = (col1+col2)/2
+	aes_score = col3
+	ces_score = (col4+col5+col6)/3
+
+	pes_percent = (pes_score * 4.16	).to_f
+	aes_percent = (aes_score * 4.16	).to_f
+	ces_percent = (ces_score * 4.16	).to_f
+
+	flag = 1
+
+	return redirect '/result'
+
 end
